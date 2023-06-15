9158f45a1c71250c82c2cf017b226980cac5807b
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-06-15 09:58:19 -0500

    Add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("UFO Sightings")
 st.header("Introduction")
-st.write("UFO's always seem to be reported, today you will find out how often it really is. This is a dashboard that shows")
-st.write("everything about UFO's and when they are seen.")
+st.write("UFO's always seem to be reported, today you will find out how often it really is. This is a dashboard that showseverything about UFO's and when they are seen.")
 df = pd.read_csv("ufo.csv")
 df1 = df
 df2 = df
@@ -16,10 +16,34 @@ st.write(df.head(10))
 
 st.header("Sightings of UFO's by Country")
 
-st.write("Below is a Bar graph and Pie chart representing how many sightings are reported on UFO's")
+st.write("Below is a Bar graph and Pie chart representing how many sightings are reported on UFO's by Countries around the world")
 
 top_5_states =df['Country'].value_counts().head(5)
-top_5_states.plot.bar()
+st.write(top_5_states.plot.bar())
 
 
+
+
+
+
+
+st.header("Average Duration of UFO Sightings")
+st.write("This histogram shows the average duration of UFO Sightings")
+
+
+#Creates the histogram with the correct range
+plt.hist(df2["Duration"], range=[0, 100])
+#Sets the title of the graph
+plt.title('Histogram: Duration of UFO Sightings')
+
+
+st.header("Hypothesis #4")
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+top_10_states = df['Shape'].value_counts().head(5)
+plt.fill_between(top_10_states.index, top_10_states.values)
+plt.title("Shapes of UFO's")
 
